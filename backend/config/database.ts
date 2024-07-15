8f35e512e0a0f713a903b00ef0eec22efbdd309f
PoonamKPatil <poonamp.dev@gmail.com>
2024-07-15 15:44:52 +0530

    added db host in env

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -7,7 +7,7 @@ require('dotenv').config();
 //create new datasource for mysql
 const appDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
